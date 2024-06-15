049cafeb551801e7b15bd29c0adf9e5a0f5462a7
DongGeun2 <ehdrms519@naver.com>
2024-06-15 16:50:44 +0900

    feat: App.jsx 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/home/HomePage";
+// Start Page
+const HomePage = lazy(() => import("./pages/home/HomePage"));
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
